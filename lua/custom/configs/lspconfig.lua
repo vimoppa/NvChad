02383a0fb61eb396594413bc3eb96497e0ab08ff
Josh <mail@chukwuanu.com>
2023-08-12 18:35:27 +0100

    feat: add custom config

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,34 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = {
+  "lua_ls",
+  "tsserver",
+  "rust_analyzer",
+  "dockerls",
+  "terraformls",
+  "awk_ls",
+  "bashls",
+  "cssls",
+  "dockerls",
+  "gopls",
+  -- "golangci_lint_ls",
+  "grammarly",
+  "graphql",
+  "jsonls",
+  "pylsp",
+  "sqlls",
+  "tailwindcss",
+  "yamlls",
+  "taplo",
+  "svelte",
+  "solidity",
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
